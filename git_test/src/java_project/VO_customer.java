046193932d3f7a0441b2236ecddef5c28a08d91a
SMT015 <SMT015@210.105.224.109>
2020-07-29 12:35:15 +0900

    VO- artist, customer, review

---
+++ git_test/src/java_project/VO_customer.java
@@ -0,0 +1,19 @@
+package java_project;
+
+public class VO_customer {
+	
+	private int done;
+
+	public int getGoal() {
+		return done;
+	}
+
+	public VO_customer(int done) {
+		super();
+		this.done = done;
+	}
+
+	public void setGoal(int goal) {
+		this.done = goal;
+	}
+}
